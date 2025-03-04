ffe54efc728f75978b7d4838e3b9f28b16c8136a
burcuusta <burcuusta93@gmail.com>
2025-03-04 12:41:12 +0300

    değişiklikler

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,68 +2,71 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(shouldWakeUp(true, 22));
+
+        System.out.println(hasTeen(8,7,15));
+
+        System.out.println(isCatPlaying(false,43));
+
+        System.out.println(area(15,20));
+        System.out.println(area(20));
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
+
+
         if (clock < 0 || clock > 23) {
             return false;
         }
-        if (isBarking) {
-            if (clock < 8 || clock >= 20) {
-                return true;
-            }
+
+        if (isBarking == true && (clock < 20 && clock > 8)) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+
+    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
+        if((firstAge<19 && firstAge>13)||(secondAge<19 && secondAge>13)||(thirdAge<19 && thirdAge>13))
+        {
+            return true;
         }
 
         return false;
     }
 
-}
-public class YasTespiti {
-    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-        return (firstAge >= 13 && firstAge <= 19) ||
-                (secondAge >= 13 && secondAge <= 19) ||
-                (thirdAge >= 13 && thirdAge <= 19);
-    }
-public static void main(String[] args) {
-    System.out.println(hasTeen(9, 99, 19));
-    System.out.println(hasTeen(23, 15, 42));
-    System.out.println(hasTeen(22, 23, 34));
-}
-     }
 
 
-public class KediOyunu {
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-        if (isSummer) {
-            return temp >= 25 && temp<= 45;
-        } else {
-            return temp >= 25 && temp<= 35;
-        }
-    }
-}
 
-public class AlanHesaplama {
+        if(isSummer==true && (temp<=45 && temp>25))
+        {
+            return true;
+        }else if(isSummer==false && (temp<=35 && temp>25))
+        {
+            return true;
+        }
+        return false;
+    }
+
     public static double area(double width, double height) {
-        if (height < 0 || width < 0) {
-            return -1.0;
+        if(width<0 || height<0)
+        {
+            return -1;
         }
-        return height * width;
+        return width*height;
+
     }
 
-
-public static double area(double radius) {
-    if (radius < 0) {
-        return -1.0;
+    public static double area(double radius) {
+        if(radius<0)
+        {
+            return -1;
+        }
+        return radius*radius*Math.PI;
     }
-    return radius * radius * Math.PI;
 }
-
-public static void main(String[] args) {
-    System.out.println(area(5.0, 4.0));
-    System.out.println(area(-1.0, 4.0));
-    System.out.println(area(5.0));
-    System.out.println(area(-1.0));
-}
-}
-
