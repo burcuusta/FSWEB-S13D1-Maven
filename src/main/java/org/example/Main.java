85cb5e208b1e3314b24226c1a08997a6c18beaf7
burcuusta <burcuusta93@gmail.com>
2025-03-04 12:29:43 +0300

    ilk kontrol
    analyzer düzenlendi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
         }
         if (isBarking) {
             if (clock < 8 || clock >= 20) {
-                return true; 
+                return true;
             }
         }
 
@@ -19,20 +19,51 @@ public class Main {
     }
 
 }
-
+public class YasTespiti {
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19);
     }
+public static void main(String[] args) {
+    System.out.println(hasTeen(9, 99, 19));
+    System.out.println(hasTeen(23, 15, 42));
+    System.out.println(hasTeen(22, 23, 34));
+}
+     }
 
+
+public class KediOyunu {
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
-    }
-
-    public static double area(double width, double height) {
-       
-    }
-
-    public static double area(double radius) {
-       
+        if (isSummer) {
+            return temp >= 25 && temp<= 45;
+        } else {
+            return temp >= 25 && temp<= 35;
+        }
     }
 }
+
+public class AlanHesaplama {
+    public static double area(double width, double height) {
+        if (height < 0 || width < 0) {
+            return -1.0;
+        }
+        return height * width;
+    }
+
+
+public static double area(double radius) {
+    if (radius < 0) {
+        return -1.0;
+    }
+    return radius * radius * Math.PI;
+}
+
+public static void main(String[] args) {
+    System.out.println(area(5.0, 4.0));
+    System.out.println(area(-1.0, 4.0));
+    System.out.println(area(5.0));
+    System.out.println(area(-1.0));
+}
+}
+
